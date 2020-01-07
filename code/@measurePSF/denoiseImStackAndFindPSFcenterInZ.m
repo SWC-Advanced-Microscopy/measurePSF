2a5fa8b68b3214e54be62fd8500c32c2e45ed4c4
Rob Campbell <git@raacampbell.com>
2020-01-07 13:24:47 +0000

    report median filtering process to screen

--- code/@measurePSF/denoiseImStackAndFindPSFcenterInZ.m
+++ code/@measurePSF/denoiseImStackAndFindPSFcenterInZ.m
@@ -1,9 +1,16 @@
 function denoiseImStackAndFindPSFcenterInZ(obj)
     % Estimate the slice that contains center of the PSF in Z by finding the brightest point.
 
+
     obj.PSFstack = double(obj.PSFstack);
-    for ii = 1:size(obj.PSFstack,3)
-        obj.PSFstack(:,:,ii) =  medfilt2(obj.PSFstack(:,:,ii),[obj.medFiltSize,obj.medFiltSize]);
+
+    if obj.medFiltSize>1
+        fprintf('median filtering PSF stack with %d by %d filter', obj.medFiltSize,obj.medFiltSize)
+        for ii = 1:size(obj.PSFstack,3)
+            fprintf('.')
+            obj.PSFstack(:,:,ii) =  medfilt2(obj.PSFstack(:,:,ii),[obj.medFiltSize,obj.medFiltSize]);
+        end
+        fprintf('\n')
     end
     obj.PSFstack = obj.PSFstack - median(obj.PSFstack(:)); %subtract the baseline because the Gaussian fit doesn't have an offset parameter
 
