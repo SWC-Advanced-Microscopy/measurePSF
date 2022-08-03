d0aba0d0e78cb29b56d0497fea15bf451a8be484
Rob Campbell <git@raacampbell.com>
2022-08-03 10:46:01 +0100

    bugfix and save images by default to current dir if it contains PSF images

--- code/+mpsf_tools/logpath.m
+++ code/+mpsf_tools/logpath.m
@@ -1,9 +1,31 @@
 function tPath=logpath
 	% Return path to which log information will be saved
-	% e.g. on a Windows machine this will be the current user's Desktop
 	%
 	% function desktop_dir = mpsf_tools.logpath
+	%
+	% Purpose
+	% This function is used by measurePSF and Grid2MicsPerPixel to choose
+	% the location to save PDF files, etc. By default we will save to the Windows
+	% Desktop, Mac Desktop, or Linux home folder. However, if the user is currently
+	% in a directory that contains the PSF recording files then we will save in
+	% the current directory instead.
+	%
+	% Inputs
+	% none
+	%
+	% Outputs
+	% tPath - path to which to save
+	%
+	%
+	% Rob Campbell - SWC 2022
 
+
+	% Are there PSF files produced by record.PSF in this folder?
+	% TODO -- what about also saving here if there are other image types (grid)?
+	if ~isempty(dir('PSF_*nm*mW_2*.tif'))
+		tPath = pwd;
+		return
+	end
 
  	
  	if ispc
@@ -13,11 +35,9 @@ function tPath=logpath
 	 	tPath = fullfile(userPath,'Desktop');
 	 elseif ismac
 	 	% Macs
-	 	tPath='~/Desktop';
+	 	tPath = '~/Desktop';
 	 elseif isunix
 	 	% Because we've already done Macs this is Linux
-	 	tPath-'~'; % In case there is no desktop folder in this distro
+	 	tPath = '~'; % In case there is no desktop folder in this distro
 	 end
 
-
-	 	
