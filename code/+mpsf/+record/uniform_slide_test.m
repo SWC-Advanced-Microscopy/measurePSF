78b7d0e17028f5993a1c2750d114c06264b38738
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2024-07-05 15:08:17 +0100

    New function to test without scan image
    Uniform_slide function without scan image code

---
+++ code/+mpsf/+record/uniform_slide_test.m
@@ -0,0 +1,28 @@
+function uniform_slide_test(varargin)
+    % Image uniform slide
+    %
+    % function mpsf.record.uniform_slide(laser_power_in_mW,laser_wavelength)
+    %
+    % User must supply power and wavelengths as integers.
+    % This is for logging purposes. If the user fails to do this,
+    % they are prompted at CLI. The order of the two arguments
+    % does not matter.
+    %
+    % e.g.
+    % >> mpsf.record.uniform_slide
+    % >> mpsf.record.uniform_slide(10,920)
+    % >> mpsf.record.uniform_slide(920,10)
+    %
+    %
+    % Rob Campbell, SWC 2022
+
+
+    params = parseInputVariable(varargin{:});
+    wavelength=params.Results.wavelength;
+    power=params.Results.power;
+
+    fprintf('wavelength is:\n ')
+    disp(wavelength)
+
+    fprintf('power is:\n')
+    disp(power)
