6bb3841c7fa501d1ec03f822d3707dab5c2312da
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2024-07-12 14:58:04 +0100

    Providing three options if no inputs are given
    Options are default value, if given string ask question again (currently not working), or give number for wavelength

--- code/+mpsf/+record/parseInputVariable.m
+++ code/+mpsf/+record/parseInputVariable.m
@@ -27,88 +27,68 @@ function out = parseInputVariable(varargin)
     params.parse(varargin{:});
 
     % Extract the variables
-    % wavelength=params.Results.wavelength
-    % power=params.Results.power
-    %  depthMicrons=params.Results.depthMicrons
-    %  stepSize=params.Results.stepSize
+    out.wavelength=params.Results.wavelength;
+    out.power=params.Results.power;
+    out.depthMicrons=params.Results.depthMicrons;
+    out.stepSize=params.Results.stepSize;
+
+
 
     d=dbstack;
     d.file;
 
 
-    if isempty(params.Results.depthMicrons) &&  strcmp(d(2).file,'PSF.m')
-        %ASK QUESTION
-        default=20;
-        response = input(sprintf('Please enter depth to image (default = %d microns): ', default));
-        out.depthMicrons = response;
-        if iesmpty(response)
-            out.depthMicrons = params.Results.depthMicrons;
-        end
-    end
-
-   if isempty(params.Results.stepSize) &&  strcmp(d(2).file,'PSF.m')
-        %ASK QUESTION
-        default=0.25;
-        response = input(sprintf('Please enter step size (default = %d microns): ', default));
-        out.stepSize = response;
-        if iesmpty(response)
-           out.stepSize = params.Results.stepSize;
-        end
-    end
+   %  if isempty(params.Results.depthMicrons) &&  strcmp(d(2).file,'PSF.m')
+   %      %ASK QUESTION
+   %      default=20;
+   %      response = input(sprintf('Please enter depth to image (default = %d microns): ', default));
+   %      out.depthMicrons = response;
+   %      if iesmpty(response)
+   %          out.depthMicrons = params.Results.depthMicrons;
+   %      end
+   %  end
+   % 
+   % if isempty(params.Results.stepSize) &&  strcmp(d(2).file,'PSF.m')
+   %      %ASK QUESTION
+   %      default=0.25;
+   %      response = input(sprintf('Please enter step size (default = %d microns): ', default));
+   %      out.stepSize = response;
+   %      if iesmpty(response)
+   %         out.stepSize = params.Results.stepSize;
+   %      end
+   %  end
 
     if isempty(params.Results.wavelength)
         %ASK QUESTION
-        % default=0.25;
-        response = input(sprintf('Please enter wavelength (nm): '));
+        default=920;
+        response = [];
+        while isempty(response) 
+            response = input(sprintf('Please enter wavelength (nm) [920]: '),'s');
+            response = str2double(response);
+             % response = str2num(response);
+            if isempty(response)
+                response = default;
+            end
+           if isnumeric(response)==0
+               response = [];
+           end
+        end
         out.wavelength = response;
-        % params.Results.wavelength = response;
-        % if iesmpty(response)
-            % params.Results.depthMicrons = default;
-        % end
     end
 
-    if isempty(params.Results.power)
-        %ASK QUESTION
-        % default=0.25;
-        response = input(sprintf('Please enter power (mW): '));
-        out.power = response;
-        % params.Results.power = response;
-        % if iesmpty(response)
-            % params.Results.depthMicrons = default;
-        % end
-    end
 
-    % if doInteractive
-    %     [laser_power_in_mW, laser_wavelength] = ineractive_nm_mW();
-    % else
-    %     laser_power_in_mW = power;
-    %     laser_wavelength = wavelength;
-    % 
+    % if isempty(params.Results.power)
+    %     %ASK QUESTION
+    %     % default=0.25;
+    %     response = input(sprintf('Please enter power (mW): '));
+    %     out.Results.power = response;
+    %     % params.Results.power = response;
+    %     % if iesmpty(response)
+    %         % params.Results.depthMicrons = default;
+    %     % end
     % end
-    % 
-    % 
-    % laser_power_in_mW = round(laser_power_in_mW);
-    % laser_wavelength = round(laser_wavelength);
 
+    
+ fprintf('wavelength is:\n');
+    disp(out.wavelength);
 
-
-    % Internal functions follow
-    % function [laser_power_in_mW, laser_wavelength] = power_wavelength_from_args(varargin)
-    %     IN = sort(cell2mat(varargin{1}));
-    %     laser_power_in_mW = IN(1);
-    %     laser_wavelength = IN(2);
-    % 
-    % 
-    % function [laser_power_in_mW, laser_wavelength] = ineractive_nm_mW()
-    % 
-    %     laser_power_in_mW = [];
-    %     while isempty(laser_power_in_mW)
-    %         txt = input('Please enter laser power in mW: ','s');
-    %         laser_power_in_mW = str2num(txt);
-    %     end
-    % 
-    %     laser_wavelength = [];
-    %     while isempty(laser_wavelength)
-    %         txt = input('Please enter laser wavelength in nm: ','s');
-    %         laser_wavelength = str2num(txt);
-    %     end
