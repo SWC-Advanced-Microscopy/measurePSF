a5d90e2c82d8a6e70d094027d9ccd4174ef770ed
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2025-01-17 16:12:28 +0000

    Update electrical_noise.m
    Added dark noise to plots, changed plot type from subplot to tiledlayout and added a global legend. Updated the function description

--- code/+mpsf/+plot/electrical_noise.m
+++ code/+mpsf/+plot/electrical_noise.m
@@ -1,58 +1,56 @@
 function electrical_noise(fname_elec, fname_dark)
-    % Electrical noise plots
-    %
-    % mpsf.plot.electrical_noise(fname)
-    %
-    % Purpose
-    % Plots of electrical noise for each channel with PMTs off and dark
-    % noise with PMTs on
-    % If there is significant electrical noise, the distributions will likely look non-Gaussian.
-    % e.g. They may look like like the sum of two Gaussians with different SDs or they
-    % may appear non-symmetric.
-    %
-    %
-    % Rob Campbell - SWC
-    % Updated by Isabell Whiteley - SWC 2025
+% Electrical and dark noise plots
+%
+% mpsf.plot.electrical_noise(fname_elec,fname_dark)
+%
+% Purpose
+% Plots of electrical noise for each channel with PMTs off and dark noise with PMTs on
+% If there is significant electrical noise, the distributions will likely look non-Gaussian.
+% e.g. They may look like like the sum of two Gaussians with different SDs or they
+% may appear non-symmetric.
+%
+%
+% Rob Campbell - SWC
+% Updated by Isabell Whiteley - SWC 2025
 
+[imstack_elec,metadata] = mpsf.tools.scanImage_stackLoad(fname_elec);
+if isempty(imstack_elec)
+    return
+end
 
-    
-    [imstack_elec,metadata] = mpsf.tools.scanImage_stackLoad(fname_elec);
-    if isempty(imstack_elec)
-        return 
-    end
+[imstack_dark,metadata] = mpsf.tools.scanImage_stackLoad(fname_dark);
+if isempty(imstack_dark)
+    return
+end
 
-    [imstack_dark,metadata] = mpsf.tools.scanImage_stackLoad(fname_dark);
-    if isempty(imstack_dark)
-        return 
-    end
+% Make a new figure or return a plot handle as appropriate
+fig = mpsf.tools.returnFigureHandleForFile([fname_elec,mfilename]);
 
-    % Make a new figure or return a plot handle as appropriate
-    fig = mpsf.tools.returnFigureHandleForFile([fname_elec,mfilename]);
+tiledlayout(2,2)
+for ii=1:size(imstack_elec,3)
+    nexttile
 
+    t_im_elec = single(imstack_elec(:,:,ii));
+    [n,x] = hist(t_im_elec(:),100);
+    a=area(x,n);
 
-    for ii=1:size(imstack_elec,3)
-        subplot(2,2,ii)
-        t_im_elec = single(imstack_elec(:,:,ii));
-        [n,x] = hist(t_im_elec(:),100);
-        a=area(x,n);
+    a.EdgeColor=[0,0,0.75];
+    a.FaceColor=[0.5,0.5,1];
+    a.LineWidth=2;
+    hold on
 
-        a.EdgeColor=[0,0,0.75];
-        a.FaceColor=[0.5,0.5,1];
-        a.LineWidth=2;
-        hold on
-        
-        t_im_dark = single(imstack_dark(:,:,ii));
-        [m,x1] = hist(t_im_dark(:),100);
-        b=area(x1,m);
+    t_im_dark = single(imstack_dark(:,:,ii));
+    [m,x1] = hist(t_im_dark(:),100);
+    b=area(x1,m);
 
-        b.EdgeColor=[0.75,0,0];
-        % b.FaceColor=[1,0.5,0.5];
-        b.FaceColor='none';
-        b.LineWidth=2;
+    b.EdgeColor=[0.75,0,0];
+    b.FaceColor='none';
+    b.LineWidth=2;
 
-
-        xlim([min(imstack_elec(:)), max(imstack_elec(:))])
-        title(sprintf('Channel %d SD=%0.2f', ii, std(t_im_dark(:))))
-        grid on
-        hold off
-    end
+    xlim([min(imstack_elec(:)), max(imstack_elec(:))])
+    title(sprintf('Channel %d SD_{dark}=%0.2f', ii, std(t_im_dark(:))))
+    grid on
+    hold off
+end
+hl = legend('Electrical Noise', 'Dark Noise', 'Orientation','horizontal');
+hl.Layout.Tile = 'south';
